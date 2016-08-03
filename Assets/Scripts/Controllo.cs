21f27a7ea1090472e31ed837cf403f9cefc6afa3
antonaccio86 <antonaccio86@gmail.com>
2016-08-03 13:16:07 +0200

    Upload new project
    Progetto di prova per verificare se unity è compatibile con github

---
+++ Assets/Scripts/Controllo.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Controllo : MonoBehaviour
+{
+	// Velocità
+	public float speed = 1.0F;
+
+	// Rotazione
+	public float rotazione = 3.0F;
+
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		CharacterController controller = GetComponent<CharacterController>();
+
+		transform.Rotate (0, Input.GetAxis ("Horizontal") * rotazione, 0);
+
+		transform.Translate (Input.GetAxis ("Vertical") * speed, 0, 0);
+
+		var forward = transform.TransformDirection (Vector3.forward);
+		var curSpeed = speed * Input.GetAxis ("Vertical");
+
+		controller.SimpleMove (forward * curSpeed);
+	}
+}
---
+++ Assets/Scripts/LoginSystem.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class LoginSystem : MonoBehaviour 
+{
+	private Rect rect;
+
+	public int xPosition = 200;
+	public int yPosition = 150;
+
+	public string username = "";
+	public string password = "";
+
+	// Update is called once per frame
+	void Update ()
+	{
+		this.rect = new Rect ((Screen.width / 2) - xPosition, (Screen.height / 2) - yPosition, 400, 300);
+	}
+
+	void OnGUI()
+	{
+		GUI.Window (0, this.rect, Login, "Login");
+	}
+
+	void Login(int id)
+	{
+		GUI.Label (new Rect (20, 50, 100, 20), "Username");
+		username = GUI.TextField (new Rect (150, 50, 200, 20), username);
+
+		GUI.Label (new Rect (20, 100, 100, 20), "Password");
+		password = GUI.PasswordField (new Rect (150, 100, 200, 20), password, char.Parse("*"));
+
+		if(GUI.Button(new Rect(20, 250, 100, 20), "Login")) {
+			if(username == "Pippo") {
+				print ("Username is " + username);
+				SceneManager.LoadScene ("Level2");
+			}
+		}
+	}
+}
